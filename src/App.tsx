025df766611fecc329a284ec173129fb5ca05894
OD <odolotov@gmail.com>
2024-02-03 21:53:29 +0700

    added gh pages deploy actions

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { initParticlesEngine } from "@tsparticles/solid/src/index";
 
 import styles from './App.module.css';
 import particlesConfig from './particles-config.json';
+import photoUrl from './assets/me.jpg';
 
 const App = () => {
     const [init, setInit] = createSignal(false);
@@ -39,11 +40,11 @@ const App = () => {
                 />
             )}
             <div class={`${styles.content} container mx-auto px-4 flex flex-col items-center max-w-7xl`}>
-                <nav class="font-bold py-4 code text-xs bg-white w-full text-center border border-slate-200">
-                    Hello world!
-                </nav>
+                <header class="font-bold py-4 text-xs bg-white w-full text-center border border-slate-200">
+                    <code>{`> Hello world! _`}</code>
+                </header>
                 <figure class="flex my-8 bg-white w-full px-4 py-2 border border-slate-200">
-                    <img class="w-32 h-32 rounded-full ml-auto mr-4 border-4 border-slate-300" src="/src/assets/me.jpg" alt="Oleg Dolotov" />
+                    <img class="w-32 h-32 rounded-full ml-auto mr-4 border-4 border-slate-300" src={photoUrl} alt="Oleg Dolotov" />
                     <figcaption class="text-lg mr-auto ml-4 py-4">
                         <h1 class="text-3xl">Oleg Dolotov</h1>
                         <h2 class="mt-4 text-2xl">Fullstack web developer with {yearsOfExperience} years of experience</h2>
