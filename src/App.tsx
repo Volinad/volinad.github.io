a8d2180e43a95bdebe498dcfee043087c9bcd911
FD <fdrdltv@gmail.com>
2024-02-14 20:20:17 +0700

    My Dad is 1901123456789 years old and I have changed colors

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,9 @@ const App = () => {
         });
     });
 
-    var age = new Date().getFullYear() - 1986;
-    var firstEmploymentDate = new Date(2009, 8, 1);
     var currentDate = new Date();
+    var age = new Date().getFullYear() - 123;
+    var firstEmploymentDate = new Date(2009, 8, 1);
     var yearsOfExperience = currentDate.getFullYear() - firstEmploymentDate.getFullYear();
 
     return (
@@ -40,15 +40,15 @@ const App = () => {
                 />
             )}
             <div class={`${styles.content} container mx-auto px-4 flex flex-col items-center max-w-7xl`}>
-                <header class="font-bold py-4 text-xs bg-white w-full text-center border border-slate-200">
-                    <code>{`> Hello world! _`}</code>
+                <header class="font-extrabold py-4 text-xs bg-orange-500 w-full text-center border border-slate-200">
+                    <code>{``}</code>Hi
                 </header>
-                <figure class="flex my-8 bg-white w-full px-4 py-2 border border-slate-200">
-                    <img class="w-32 h-32 rounded-full ml-auto mr-4 border-4 border-slate-300" src={photoUrl} alt="Oleg Dolotov" />
+                <figure class="flex my-8 bg-amber-600 w-full px-4 py-2 border border-slate-200">
+                    <img class="w-32 h-32 rounded-3xl ml-auto mr-4 border-4 border-slate-300" src={photoUrl} alt="Oleg Dolotov" />
                     <figcaption class="text-lg mr-auto ml-4 py-4">
-                        <h1 class="text-3xl">Oleg Dolotov</h1>
+                        <h1 class="text-3xl">Bob</h1>
                         <h2 class="mt-4 text-2xl">Fullstack web developer with {yearsOfExperience} years of experience</h2>
-                        <p class="text-sm">{age} years old, digital nomad, currently living in Thailand</p>
+                        <p class="text-sm">{age} 123456789 years old, digital nomad, currently living in Thailand</p>
                     </figcaption>
                 </figure>
             </div>
